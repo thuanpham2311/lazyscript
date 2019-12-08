a35ec2da358d432c013e641cfdd8df58f0da58b3
nicholas <nicholaspham2311@gmail.com>
2019-12-08 21:59:19 +0700

    This should work until december 2013.

--- rbn.bash
+++ rbn.bash
@@ -1,3 +1,7 @@
+pactl set-card-profile 0 output:hdmi-stereo
+gnome-terminal
+google-chrome --new-window
+
 sudo snap refresh
 sudo apt-get update
 sudo apt-get upgrade -y
@@ -5,8 +9,16 @@ sudo apt-get autoremove -y
 sudo apt-get autoclean
 sudo apt-get clean
 nvim -c "PlugUpdate | qa"
-notify-send "software fresh"
+notify-send -i ~/git/lazyscript/icon/update.png "fresh"
 
+
+cd ~/tools/hacking/sherlock/ ; git pull
+cd ~/git/dotfiles ; git pull
+cd ~/git/dotfiles-termux ; git pull
+cd ~/git/study ; git pull
+cd ~/git/mywebsite ; git pull
+cd ~/git/pomodoro ; git pull
+cd ~/git/lazyscript ; git pull
 
 cp ~/.gitconfig  ~/git/dotfiles/git/ ; cp ~/.tmux.conf ~/git/dotfiles/tmux ; cat ~/.config/nvim/init.vim > ~/.vimrc ; cp ~/.vimrc ~/git/dotfiles/vim ; cp ~/.config/nvim/init.vim ~/git/dotfiles/nvim ; cp ~/.bashrc ~/git/dotfiles/bash ; cp ~/.config/fish/config.fish ~/git/dotfiles/fish/ ; cp ~/.config/vifm/vifmrc ~/git/dotfiles/vifm/ ; cp ~/.config/vifm/colors/Default.vifm ~/git/dotfiles/vifm/colors/ ; cp ~/.ssh/config ~/git/dotfiles/ssh/ ; cd ~/git/dotfiles ; 
 git add -A ; git commit -m "$(curl -s whatthecommit.com/index.txt)"
@@ -32,11 +44,4 @@ cd ~/git/lazyscript ; git add -A
 git commit -m "$(curl -s whatthecommit.com/index.txt)"
 git push
 
-cd ~/tools/hacking/sherlock/ ; git pull
-cd ~/git/dotfiles ; git pull
-cd ~/git/dotfiles-termux ; git pull
-cd ~/git/study ; git pull
-cd ~/git/mywebsite ; git pull
-cd ~/git/pomodoro ; git pull
-cd ~/git/lazyscript ; git pull
-notify-send "git fresh"
+notify-send -i ~/git/lazyscript/icon/github.png "fresh"
