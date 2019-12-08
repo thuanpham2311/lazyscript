322fae0609770051c5e279bada9826a36583dac9
nicholas <nicholaspham2311@gmail.com>
2019-12-08 21:30:18 +0700

    that's all folks

--- startup.bash
+++ startup.bash
@@ -4,6 +4,9 @@ sudo apt-get upgrade -y
 sudo apt-get autoremove -y
 sudo apt-get autoclean
 sudo apt-get clean
+nvim -c "PlugUpdate | qa"
+notify-send "software fresh"
+
 
 cp ~/.gitconfig  ~/git/dotfiles/git/ ; cp ~/.tmux.conf ~/git/dotfiles/tmux ; cat ~/.config/nvim/init.vim > ~/.vimrc ; cp ~/.vimrc ~/git/dotfiles/vim ; cp ~/.config/nvim/init.vim ~/git/dotfiles/nvim ; cp ~/.bashrc ~/git/dotfiles/bash ; cp ~/.config/fish/config.fish ~/git/dotfiles/fish/ ; cp ~/.config/vifm/vifmrc ~/git/dotfiles/vifm/ ; cp ~/.config/vifm/colors/Default.vifm ~/git/dotfiles/vifm/colors/ ; cp ~/.ssh/config ~/git/dotfiles/ssh/ ; cd ~/git/dotfiles ; 
 git add -A ; git commit -m "$(curl -s whatthecommit.com/index.txt)"
@@ -36,3 +39,4 @@ cd ~/git/study ; git pull
 cd ~/git/mywebsite ; git pull
 cd ~/git/pomodoro ; git pull
 cd ~/git/lazyscript ; git pull
+notify-send "git fresh"
