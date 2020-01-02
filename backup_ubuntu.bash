28690753ab357502aee627518cf80019bbeb639c
nicholas <nicholaspham2311@gmail.com>
2020-01-02 21:32:48 +0700

    whooooooooooooooooooooooooooo

--- backup_ubuntu.bash
+++ backup_ubuntu.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # necessary
-sudo apt-get install ttf-mscorefonts-installer neovim tmux curl wget git fortune fish neofetch htop mpv gnome-tweaks cowsay trash-cli openssh-client openssh-server ibus-unikey flameshot vifm audacity python3-pip python-pip tldr simplescreenrecorder net-tools npm nodejs nmap whois cmus bucklespring libreoffice gconftool audacity gimp preload olive-editor transmission krita xclip -y
+sudo apt-get install ttf-mscorefonts-installer neovim tmux curl wget git fortune fish neofetch htop mpv gnome-tweaks cowsay trash-cli openssh-client openssh-server ibus-unikey flameshot vifm audacity python3-pip python-pip tldr simplescreenrecorder net-tools npm nodejs nmap whois cmus bucklespring libreoffice gconftool audacity gimp preload olive-editor transmission krita xclip cmatrix -y
 ibus restart
 chsh -s "$(which fish)"
 
@@ -12,6 +12,7 @@ rm -rf google-chrome-stable_current_amd64.deb
 
 # Python setup
 sudo apt-get install -y python3-pandas
+sudo apt-get install python3-matplotlib
 pip3 install notebook
 pip3 install pylint
 pip3 install numpy
