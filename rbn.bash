5249a7def331eaba388d117b7851647f00b48a1d
nicholas <nicholaspham2311@gmail.com>
2020-01-10 07:53:45 +0700

    Major fixup.

--- rbn.bash
+++ rbn.bash
@@ -1,11 +1,2 @@
-gnome-terminal
-
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get autoremove -y
-sudo apt-get autoclean
-sudo apt-get clean
-nvim -c "PlugUpdate | qa"
-notify-send -i ~/git/lazyscript/icon/update.png "fresh"
-
 pactl set-card-profile 0 output:hdmi-stereo
+nvim -c "PlugUpdate | qa"
