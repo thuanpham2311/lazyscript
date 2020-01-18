cc5a81c8e8fda06693ee5cb81973cf4abae6307d
nicholas <nicholaspham2311@gmail.com>
2020-01-18 10:20:02 +0700

    Replace all whitespaces with tabs.

--- backup_ubuntu.bash
+++ backup_ubuntu.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 # necessary
-sudo apt-get install ttf-mscorefonts-installer tmux curl wget git fortune fish neofetch htop mpv gnome-tweaks cowsay trash-cli openssh-client openssh-server ibus-unikey flameshot audacity python3-pip python-pip tldr simplescreenrecorder net-tools npm nodejs nmap whois bucklespring libreoffice gconftool audacity gimp preload olive-editor krita xclip speedtest-cli mysql-server neovim fdfind aria2c tree -y
+sudo apt-get install ttf-mscorefonts-installer tmux curl wget git fortune fish neofetch htop mpv gnome-tweaks cowsay trash-cli openssh-client openssh-server ibus-unikey flameshot audacity python3-pip python-pip tldr simplescreenrecorder net-tools npm nodejs nmap whois bucklespring libreoffice gconftool audacity gimp preload olive-editor krita xclip speedtest-cli mysql-server neovim fdfind aria2c tree simplescreenrecorder olive-editor libreoffice -y
 sudo snap innstall grv
 ibus restart
 
@@ -10,14 +10,12 @@ sudo dpkg -i google-chrome-stable_current_amd64.deb
 rm -rf google-chrome-stable_current_amd64.deb
 
 # Python setup
-sudo apt-get install -y python3-pandas
-sudo apt-get install python3-matplotlib
-sudo apt-get install python3-pygame
+sudo apt-get install -y python3-pandas python3-matplotlib python3-pygame
 sudo pip install csvkit
+sudo pip install opencv-python
 sudo pip3 install notebook
 sudo pip3 install pylint
 sudo pip3 install numpy
-sudo pip install opencv-python
 
 # nvim setup
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
