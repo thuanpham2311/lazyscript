df1284fc191ad9d86bc5e80feeeb7732b25be24b
nicholas <nicholaspham2311@gmail.com>
2020-01-28 11:18:57 +0700

    For the statistics only

--- backup_ubuntu.bash
+++ backup_ubuntu.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 # necessary
-sudo apt-get install ttf-mscorefonts-installer tmux curl wget git fortune fish neofetch htop mpv gnome-tweaks cowsay trash-cli openssh-client openssh-server ibus-unikey flameshot audacity python3-pip python-pip tldr simplescreenrecorder net-tools npm nodejs nmap whois bucklespring libreoffice gconftool audacity gimp preload krita xclip speedtest-cli mysql-server neovim fdfind aria2c tree libreoffice -y
+sudo apt-get install ttf-mscorefonts-installer tmux curl wget git fish neofetch htop mpv gnome-tweaks trash-cli openssh-client openssh-server ibus-unikey flameshot python3-pip python-pip tldr net-tools libreoffice preload krita xclip speedtest-cli mysql-server neovim fd-find aria2c tree -y
 sudo snap innstall grv
 ibus restart
 
