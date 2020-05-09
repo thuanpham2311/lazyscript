987eab1406e2dee95de7b03e2eec410c4fc059ab
nicholas <nicholaspham2311@gmail.com>
2020-05-09 21:36:17 +0700

    Whee, good night.

---
+++ phamTanThuanDH51902283_nhapMonLapTrinh_week3.cpp
@@ -0,0 +1,62 @@
+/**
+ *     Author: Nicholas Pham (Phạm Tấn Thuận | DH51902283)
+ *     Created: Sat 09 May 2020 09:27:29 PM +07
+**/
+// README:
+// # See the latest update at:
+// https://github.com/nicholaspham2311/lazyscript/blob/master/phamTanThuanDH51902283_nhapMonLapTrinh_week3.cpp
+//
+//
+// NOTE:
+// 1. Viết chương trình nhập vào 3 số thực a, b, c. Tìm số lớn nhất và nhỏ nhất trong 3 số a, b, c.
+// 2. Viết chương trình nhập vào số nguyên n, tính :
+// a. S= 1 – 2 + 3 – 4 + 5 - .... ± n
+// b.
+// 3. Viết chương trình nhập vào 2 số nguyên dương x, y. Tính x^y .
+//
+//
+// TODO:
+// [ ] ex1
+// using if
+// [ ] ex2
+// -= 1 += 1 to X
+// eg. x = 10, 1=>10 = 10bit.
+// [ ] ex3
+// ??? cin x, y? solve = x**Y(python) cpp?
+
+#include <iostream>
+
+void ex1() {
+
+}
+
+void ex2() {
+
+}
+
+void ex3() {
+
+}
+
+int main() {
+
+	cout <<   "_____________"          << endl;
+	cout <<   "< Yo! dudes >"          << endl;
+	cout <<   "-------------"          << endl;
+	cout <<   "  |  ^__^"              << endl;
+	cout <<   "   - (**)|_______  #"   << endl;
+	cout <<   "     (__)|       )/"    << endl;
+	cout <<   "         ||----w |"     << endl;
+	cout <<   "         ||     ||"     << endl;
+
+	cout << "\n\n Exercise 1" << endl;
+	ex1();
+
+	// cout << "\n\n Exercise 2" << endl;
+	// ex2();
+
+	// cout << "\n\n Exercise 3" << endl;
+	// ex3();
+
+	return 0;
+}
