73affa6e91dd57fdabc3d60350eef8ce2e640816
thuan <phamtanthuan2311@gmail.com>
2020-07-04 23:34:55 +0700

    move your body every every body

---
+++ nhapMonLapTrinh/phamTanThuanDH51902283_nhapMonLapTrinh_week12/README.txt
@@ -0,0 +1,2 @@
+See newest update at:
+
--- nhapMonLapTrinh/phamTanThuanDH51902283_nhapMonLapTrinh_week12/main.cpp
+++ nhapMonLapTrinh/phamTanThuanDH51902283_nhapMonLapTrinh_week12/main.cpp
@@ -8,5 +8,16 @@ int main() {
     cin >> n;
     nhapDanhSachSach(a, n);
 
+    cout << "Cac quyen sach xuat ban nam 2020:" << endl;
+    xuatDSSach2020(a, n);
+
+    cout << "Cac quyen sach co gia 100K toi 300K:" << endl;
+    xuatDSSachGiaFrom100kTo300k(a, n);
+
+    cout << "Cac quyen sach co ma bat dau bang T la:" << endl;
+    xuatDSSachMaT(a, n);
+
+    cout << "Sach co ma \"TH001\" la:";
+    xuatSachMaTH001(a, n);
     return 0;
 }
