ea8bd107535fe988e3815b79fc8c015dafd1a258
thuan <phamtanthuan2311@gmail.com>
2020-07-04 23:00:35 +0700

    Added a banner to the default admin page. Please have mercy on me =(

--- nhapMonLapTrinh/phamTanThuanDH51902283_nhapMonLapTrinh_week12/main.cpp
+++ nhapMonLapTrinh/phamTanThuanDH51902283_nhapMonLapTrinh_week12/main.cpp
@@ -1,5 +1,12 @@
 #include "header.h"
 
 int main() {
+    SACH a[100];
+    int n;
+
+    cout << "Nhap so luong sach: ";
+    cin >> n;
+    nhapDanhSachSach(a, n);
+
     return 0;
 }
--- nhapMonLapTrinh/phamTanThuanDH51902283_nhapMonLapTrinh_week12/source.cpp
+++ nhapMonLapTrinh/phamTanThuanDH51902283_nhapMonLapTrinh_week12/source.cpp
@@ -1,2 +1,31 @@
 #include "header.h"
 
+void nhapSach(SACH &sach) {
+    cout << "Nhap ten: ";
+    cin.getline(sach.ten, 50);
+    cin.ignore();
+    cout << "Nhap ma: ";
+    cin >> sach.ma;
+    cout << "Nhap gia: ";
+    cin >> sach.gia;
+    cout << "Nhap nam: ";
+    cin >> sach.nam;
+}
+
+void xuatSach(SACH sach) {
+    cout << endl;
+
+    cout << "Ten sach: " << sach.ten << endl;
+    cout << "Ten ma: " << sach.ma << endl;
+    cout << "Ten gia: " << sach.gia << endl;
+    cout << "Ten nam: " << sach.nam << endl;
+
+    cout << endl;
+}
+
+void nhapDanhSachSach(SACH a[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << "Nhap sach thu " << i << ":" << endl;
+        nhapSach(a[i]);
+    }
+}
