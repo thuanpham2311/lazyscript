d1a3fe4077a2320b287be88dce6fdd5c6def79b8
thuan <phamtanthuan2311@gmail.com>
2020-07-16 08:52:22 +0700

    somebody keeps erasing my changes.

--- onNhapMonLapTrinh/kiemTraSoChinhPhuong.cpp
+++ onNhapMonLapTrinh/kiemTraSoChinhPhuong.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 // https://vi.wikipedia.org/wiki/S%E1%BB%91_ch%C3%ADnh_ph%C6%B0%C6%A1ng#:~:text=S%E1%BB%91%20ch%C3%ADnh%20ph%C6%B0%C6%A1ng%20hay%20c%C3%B2n,c%E1%BA%A1nh%20b%E1%BA%B1ng%20s%E1%BB%91%20nguy%C3%AAn%20kia.
+
 int main() {
     int n;
     bool ok;
--- onNhapMonLapTrinh/kiemTraSoNguyenTo.cpp
+++ onNhapMonLapTrinh/kiemTraSoNguyenTo.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 // https://vi.wikipedia.org/wiki/S%E1%BB%91_nguy%C3%AAn_t%E1%BB%91#:~:text=C%C3%A1c%20s%E1%BB%91%20nguy%C3%AAn%20t%E1%BB%91%20t%E1%BB%AB%202%20%C4%91%E1%BA%BFn%20100%3A,%2C%2083%2C%2089%2C%2097.
+
 int main() {
     int n;
     int count = 0;
