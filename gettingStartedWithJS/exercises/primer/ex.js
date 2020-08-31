1309c02affa57302b3413eca30e2f7d8b9007a71
thuan <phamtanthuan2311@gmail.com>
2020-08-31 19:00:44 +0700

    SOAP is a piece of shit

--- gettingStartedWithJS/exercises/primer/ex.js
+++ gettingStartedWithJS/exercises/primer/ex.js
@@ -4,8 +4,18 @@
 
 var favoriteBooks = [];
 
-function addFavoriteBook(book) {
-    favoriteBooks.push(book);
+function addFavoriteBook(bookName) {
+    let check;
+    for (let i = 0; i <= favoriteBooks.length; i++) {
+        if (bookName.includes(favoriteBooks[i]) === true) {
+            check = true;
+        }
+    }
+    if (check === true) {
+        return;
+    } else {
+        return favoriteBooks.push(bookName);
+    }
 }
 
 function printFavoriteBooks(bookNumber) {
@@ -16,8 +26,11 @@ addFavoriteBook("A Song of Ice and Fire");
 addFavoriteBook("The Great Gatsby");
 addFavoriteBook("Crime & Punishment");
 addFavoriteBook("Great Expectations");
+addFavoriteBook("Great Expectations");
 addFavoriteBook("You Don't Know JS");
 
 // TODO: print out favorite books
 
-printFavoriteBooks();
+for (let i = 0; i <= favoriteBooks.length; i++) {
+    console.log(favoriteBooks[i]);
+}
