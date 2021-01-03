ccc4562c104cbf7c237e90f9606c0dcf2d33c38d
thuan <phamtanthuan2311@gmail.com>
2021-01-03 19:02:46 +0700

    They came from... Behind

--- thucHanhNhapMonLapTrinh/baiTap1THNMLT.cpp
+++ thucHanhNhapMonLapTrinh/baiTap1THNMLT.cpp
@@ -1,7 +1,7 @@
 /**
  *     Author: Nicholas Pham (Phạm Tấn Thuận | DH51902283)
  *     Created: Mon 01 Jun 2020 05:58:22 PM +07
-**/
+ **/
 // README:
 // # See the latest update at:
 // https://github.com/thuanpham2311/lazyscript/blob/master/thucHanhNhapMonLapTrinh/baiTap1THNMLT.cpp
@@ -15,32 +15,32 @@ using namespace std;
 
 int main() {
 
-    cout <<   "_____________"          << endl;
-    cout <<   "< Yo! dudes >"          << endl;
-    cout <<   "-------------"          << endl;
-    cout <<   "  |  ^__^"              << endl;
-    cout <<   "   - (oo)|_______  #"   << endl;
-    cout <<   "     (__)|       )/"    << endl;
-    cout <<   "         ||----w |"     << endl;
-    cout <<   "         ||     ||"     << endl;
+  cout << "_____________" << endl;
+  cout << "< Yo! dudes >" << endl;
+  cout << "-------------" << endl;
+  cout << "  |  ^__^" << endl;
+  cout << "   - (oo)|_______  #" << endl;
+  cout << "     (__)|       )/" << endl;
+  cout << "         ||----w |" << endl;
+  cout << "         ||     ||" << endl;
 
-    int n, sum = 0;
+  int n, sum = 0;
 
-    cout << "Enter n = ";
-    cin >> n;
+  cout << "Enter n = ";
+  cin >> n;
 
-    // nguyen duong n dung do while kiem tra n > 0
+  // nguyen duong n dung do while kiem tra n > 0
 
-    int r = 0;
-    while (n > 0) {
-        r = n % 10;
-        if ( r % 2 == 0 ) {
-            sum += r;
-        }
-        n /= 10;
+  int r = 0;
+  while (n > 0) {
+    r = n % 10;
+    if (r % 2 == 0) {
+      sum += r;
     }
+    n /= 10;
+  }
 
-    cout << "\n Sum = " << sum << endl;
+  cout << "\n Sum = " << sum << endl;
 
-    return 0;
+  return 0;
 }
